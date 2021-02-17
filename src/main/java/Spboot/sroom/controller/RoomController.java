62dcbf4dddafc16b5fc43ce16dfb7dad41695a6a
1kimm <anyionee@naver.com>
2021-02-17 15:17:49 +0900

    modify roomController by 1kimm

--- src/main/java/Spboot/sroom/controller/RoomController.java
+++ src/main/java/Spboot/sroom/controller/RoomController.java
@@ -21,9 +21,7 @@ public class RoomController {
 	
 	@RequestMapping(value="/listRoom", method={RequestMethod.GET})
     public List<RoomVO> listAll(Model model) {
-		System.out.print("서버연결");
 		List<RoomVO> rooms = rs.getAllRoom();
-		System.out.print("방 목록:"+rooms);
 		return rooms;
 	}
 	
