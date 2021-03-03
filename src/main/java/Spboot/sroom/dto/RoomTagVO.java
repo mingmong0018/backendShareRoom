81b2a872e93e4689b10d5e0041f0959bc7b2e883
mingmong0018 <mingmong0018@google.com>
2021-03-03 15:58:32 +0900

    2021-03-03

--- src/main/java/Spboot/sroom/dto/RoomTagVO.java
+++ src/main/java/Spboot/sroom/dto/RoomTagVO.java
@@ -6,4 +6,16 @@ import lombok.Data;
 public class RoomTagVO {
 	private int room_id;
 	private String tag_content;
+	public int getRoom_id() {
+		return room_id;
+	}
+	public void setRoom_id(int room_id) {
+		this.room_id = room_id;
+	}
+	public String getTag_content() {
+		return tag_content;
+	}
+	public void setTag_content(String tag_content) {
+		this.tag_content = tag_content;
+	}
 }
