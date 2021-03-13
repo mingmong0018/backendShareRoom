27d494fcb4bc229236a4f69a7f2392ff128b2514
mingmong0018 <mingmong0018@google.com>
2021-03-13 20:33:07 +0900

    re-build modify host by ming

--- src/main/java/Spboot/sroom/config/WebConfig.java
+++ src/main/java/Spboot/sroom/config/WebConfig.java
@@ -2,11 +2,14 @@ package Spboot.sroom.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import Spboot.sroom.config.interceptor.JwtAuthIntercepter;
 
 @Component
+@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer{
 	
 	@Autowired
@@ -15,6 +18,12 @@ public class WebConfig implements WebMvcConfigurer{
 	private String[] INTERCEPTOR_WHITE_LIST= {
 			"/login/**","/upload/**",
 	};
+	@Override
+	public void addCorsMappings(CorsRegistry cr) {
+		cr.addMapping("/**")
+		.allowedOrigins("http://http://shareroom-frontend.s3-website.ap-northeast-2.amazonaws.com:80")  // 허용할 주소 및 포트
+		.allowedOrigins("http://localhost:8081");  // 허용할 주소 및 포트
+	}
 
 	
 	@Override
